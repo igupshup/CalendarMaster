3aae0ca85bc9a5946cbb67b88d4496ab3508c997
igupshup <42326275+igupshup@users.noreply.github.com>
2024-04-22 18:34:11 +0530

    Update CalendarGridView.java

--- CalendarGridView.java
+++ CalendarGridView.java
@@ -1,1 +1,30 @@
+package com.applandeo.materialcalendarview.extensions;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * Created by Applandeo Team.
+ */
+
+public class CalendarGridView extends GridView {
+
+    public CalendarGridView(Context context) {
+        super(context);
+    }
+
+    public CalendarGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CalendarGridView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    //This method is needed to get wrap_content height for GridView
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST));
+    }
+}
